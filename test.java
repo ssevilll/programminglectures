0b5443af40c4429b52ad3fa3d9244dfc84634f34
Nargiz Mammadzada <nargizhma@code.edu.az>
2026-03-12 13:05:14 +0400

    default, implement, umumi interface

--- test.java
+++ test.java
@@ -1,11 +1,19 @@
 class test{
     public static void main(String args[]) {
      System.out.println("Hello World!");
+        C obj = new C();
+        obj.show();
+        obj.grow();
+        obj.display();
     }
 
 }
 interface A{
     void show();
+    default void display(){ // hem default hem static hem abstract olmur.
+        //default methodlar interface icinde body alabilirler.
+        System.out.println("Welcome");
+    }
 }
 interface B{
     void grow();
@@ -17,4 +25,6 @@ class C implements A,B{
     public void grow(){
         System.out.println("World");
     }
+    // display methodunu override etmedik cunku default olarak interface icinde tanimladik.
+    // is it like inheriting parent class method? Yes, it is like inheriting parent class method.
 }
