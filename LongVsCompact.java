c478aa13fd45ba2fedd9afe05f4a883f3be9eb3e
Nargiz Mammadzada <nargizhma@code.edu.az>
2026-03-12 16:12:57 +0400

    record -> long and compact constructor

--- LongVsCompact.java
+++ LongVsCompact.java
@@ -6,9 +6,9 @@ public class LongVsCompact {
         // what is accessor methods and why because of that component is method? Accessor methods are methods that allow you to access the values of the components of a record. In a record, each component is automatically given an accessor method with the same name as the component. So, in this case, accountType() is an accessor method that allows you to access the value of the accountType component of the Account record.
     }
 }
-record Account (String customerName, double balance, String accountType) {
+record Account (String customerName, double balance, String accountType) { // when declaring record, there has to be inputs in the parentheses. These are called components. They are like fields in a class, but they are final and immutable. They are also automatically given accessor methods with the same name as the component.
 
-    public Account{
+    public Account{ // compact constructor only exists in record.
         customerName = customerName == null || customerName.trim().isEmpty()? "Unnamed Customer" : customerName;
         customerName = customerName.substring(0,1).toUpperCase() + customerName.substring(1).toLowerCase();
         balance = balance < 0 ? 0 : balance;
